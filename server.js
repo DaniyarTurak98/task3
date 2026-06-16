198c165235ff99d73b7ace456b6a536eb8c3bf31
Daniyar Turak <daniyarturak@MacBook-Air-Daniyar.local>
2026-06-16 15:02:52 +0500

    fix

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.get("/daniyarturak_gmail_com", (req, res) => {
     const y = Number(req.query.y);
 
     if (!Number.isInteger(x) || !Number.isInteger(y) || x <= 0 || y <= 0) {
-        return res.json("NaN");
+        return res.send("NaN");
     }
 
     const gcd = (a, b) => (b === 0 ? a : gcd(b, a % b));
     const lcm = (x * y) / gcd(x, y);
 
-    res.json(lcm);
+    return res.send(String(lcm));
 });
 
 app.listen(PORT, () => {
